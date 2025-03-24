4e6712404602a516f30347046d0a753eb9d7ef43
Nethmamlshni <nethmamalshani2002@gmail.com>
2025-03-24 14:28:16 +0530

    allow frontend CORS

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import Userouter from './routes/userRoute.js';
+import Userrouter from './routes/userRoute.js';
 import Bookingrouter from './routes/bookingRoute.js';
 import Feedbackrouter from './routes/feedbackRoute.js';
 import Inforrouter from './routes/informationRoute.js';
@@ -13,25 +13,46 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-app.use(cors());
-app.use(bodyParser.json());
-app.use('/api/users', Userouter);
-app.use('/api/bookings',Bookingrouter);
-app.use('/api/feedbacks',Feedbackrouter);
-app.use('/api/information',Inforrouter);
-app.use('/api/price',Pricerouter);
-app.use('/api/servises',Servicerouter);
 
-const connect =process.env.Mongo_Url; ;
+
+const allowedOrigins = ['https://saloon-frontend-new.vercel.app']; 
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+        callback(null, true); // Allow request
+      } else {
+        callback(new Error('Not allowed by CORS'), false); // Reject request
+      }
+    },
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: "Content-Type,Authorization",
+  })
+);
+
+app.use(bodyParser.json());
+
+// Routes
+app.use('/api/users', Userrouter);
+app.use('/api/bookings', Bookingrouter);
+app.use('/api/feedbacks', Feedbackrouter);
+app.use('/api/information', Inforrouter);
+app.use('/api/price', Pricerouter);
+app.use('/api/servises', Servicerouter);
+
+// MongoDB connection
+const connect = process.env.Mongo_Url;
 
 mongoose.connect(connect)
-    .then(() => {
-        console.log('Connected to MongoDB');
-    })
-    .catch((err) => {
-        console.error('MongoDB connection error:', err);
-    });
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error('MongoDB connection error:', err);
+  });
 
-app.listen(3000, (req,res) => {
-    console.log('Server is running on port 3000');
-    } );
+// Server listener
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
